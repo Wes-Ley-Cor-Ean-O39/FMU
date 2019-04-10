cba85461b9df157a49ac76ef9f70653a43f3dbc9
Wesley Azevedo <wesleycoreano39@gmail.com>
2019-04-09 22:24:54 -0300

    alura / byteBank-herdado-conta (Construtor / Override)

---
+++ bytebank-herdado-conta/src/ContaPoupança.java
@@ -0,0 +1,8 @@
+
+public class ContaPoupança extends Conta {
+
+	public ContaPoupança(int agencia, int numero) {
+		super(agencia, numero);
+	}
+
+}
---
+++ bytebank-herdado-conta/src/TesteContas.java
@@ -0,0 +1,18 @@
+
+public class TesteContas {
+
+	public static void main(String[] args) {
+
+		ContaCorrente cc = new ContaCorrente(111, 111);
+		cc.deposita(100.0);
+
+		ContaPoupança cp = new ContaPoupança(222, 222);
+		cp.deposita(100.0);
+
+		cc.transfere(10.0, cp);
+		System.out.println("CC: " + cc.getSaldo());
+		System.out.println("CP: " + cp.getSaldo());
+
+	}
+
+}
