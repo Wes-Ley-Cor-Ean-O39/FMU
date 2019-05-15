ff0e2a427ebae8b0d2e06260034b5080c13233c4
Wesley Azevedo <wesleycoreano39@gmail.com>
2019-05-14 21:15:39 -0300

    Java parte 7: Trabalhando com java.io / Leitura com java.io

---
+++ java-io/src/br/com/alura/java/io/teste/TesteLeitura.java
@@ -0,0 +1,26 @@
+package br.com.alura.java.io.teste;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TesteLeitura {
+
+	public static void main(String[] args) throws IOException {
+
+		FileInputStream fis = new FileInputStream("lorem.txt");
+		InputStreamReader isr = new InputStreamReader(fis);
+		BufferedReader br = new BufferedReader(isr);
+
+		String linha = br.readLine();
+
+		while (linha != null) {
+			System.out.println(linha);
+			linha = br.readLine();
+		}
+
+		br.close();
+
+	}
+}
