3cff29329668effd40ada5d15da441d6b98a68c5
Wesley Azevedo <wesleycoreano39@gmail.com>
2019-06-06 21:04:01 -0300

    ant build.xml

---
+++ treinamento/src/treinamento/Pessoa.java
@@ -0,0 +1,29 @@
+package treinamento;
+
+import com.thoughtworks.xstream.XStream;
+
+public class Pessoa {
+	private String nome;
+	private int idade;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String toXML() {
+		XStream stream = new XStream();
+		return stream.toXML(this);
+	}
+}
