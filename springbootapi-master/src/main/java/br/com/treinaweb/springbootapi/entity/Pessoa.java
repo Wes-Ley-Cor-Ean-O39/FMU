2614b444905fc5407699e556e3218128ca42b24b
Wesley Azevedo <wesleycoreano39@gmail.com>
2020-01-11 12:42:08 -0300

    Api Rest Java com Spring Boot

---
+++ springbootapi-master/src/main/java/br/com/treinaweb/springbootapi/entity/Pessoa.java
@@ -0,0 +1,38 @@
+package br.com.treinaweb.springbootapi.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import io.swagger.annotations.ApiModelProperty;
+
+@Entity
+public class Pessoa
+{
+    @ApiModelProperty(value = "Código da pessoa")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @ApiModelProperty(value = "Nome da pessoa")
+    @Column(nullable = false)
+    private String nome;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
